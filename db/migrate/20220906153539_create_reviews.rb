8e01d2973943d1e2f157672c3951dda45614f056
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-09-06 20:18:17 +0300

    Adds the datbase with the tables together.

--- db/migrate/20220906153539_create_reviews.rb
+++ db/migrate/20220906153539_create_reviews.rb
@@ -1,4 +1,11 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
+    create_table :reviews do |t|
+      t.string  :comment
+      t.string  :image_url
+      t.string  :trading_pair
+      t.integer :user_id
+      t.timestamps
+    end
   end
 end
