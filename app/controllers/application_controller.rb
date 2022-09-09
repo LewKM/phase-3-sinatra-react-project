0705a8290b029678c8d65b8904f382f338802754
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-09-09 04:54:41 +0300

    Done testing the end point on post man, Starts the frontend intergration.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,34 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
+  get "/reviews" do
     # gets all the items from the database
     # and returns them as a JSON array
-    Item.all.to_json
+    Review.all.to_json
   end
+  
+    # POST /reviews
+  
+    post '/reviews' do
+      review = Review.create(params)
+      review.to_json
+    end
+  
+    # PATCH /reviews/:id
+  
+    patch '/reviews/:id' do
+      review = Review.find(params[:id])
+      review.update(params)
+      review.to_json
+    end
+    
+    # DELETE /reviews/:id
+  
+    delete '/reviews/:id' do
+      review = Review.find(params[:id])
+      review.destroy
+      review.to_json
+      "Review deleted successfully"
+    end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,10 @@
 class Review < ActiveRecord::Base
-    belongs_to :user    
+    belongs_to :user   
+
+    def username
+        # self is the review instance
+        User.find(self.user_id)
+    end
+    
+
 end
